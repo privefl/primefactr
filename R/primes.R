bfe3f5b61baecd52f3c3935ddae3a573722173ef
Florian <florian.prive.21@gmail.com>
2016-08-21 08:58:49 +0200

    optimize code

--- R/primes.R
+++ R/primes.R
@@ -32,7 +32,7 @@ AllPrimesUpTo <- function(n) {
   primes <- rep(TRUE, n)
   primes[1] <- FALSE
   for (i in 1:sqrt(n)) {
-    if (primes[i]) primes[seq(2*i, n, i)] <- FALSE
+    if (primes[i]) primes[seq(i^2, n, i)] <- FALSE
   }
   which(primes)
 }
